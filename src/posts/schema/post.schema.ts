2e33ddae0abc78e768d83f57bef2e5e69771931a
kamarini09 <moragiannikam@gmail.com>
2024-11-22 16:21:48 +0100

    change the schema

--- src/posts/schema/post.schema.ts
+++ src/posts/schema/post.schema.ts
@@ -3,25 +3,31 @@ import * as mongoose from 'mongoose';
 import { Types } from 'mongoose';
 import { Ensemble } from 'src/ensembles/schema/ensemble.schema';
 import { User } from 'src/users/schema/user.schema';
-
+export interface Instrument {
+	name: string;
+	level: number;
+	genre: string;
+}
 export type PostDocument = Post & Document;
 
-@Schema()
+@Schema({ timestamps: true }) // Automatically adds createdAt and updatedAt
 export class Post {
-
 	readonly _id: Types.ObjectId;
-	
+
 	@Prop({ required: true })
 	title: string;
 
 	@Prop({ required: true })
-	content: string;
+	description: string;
+
+	@Prop({ type: [{ name: String, level: Number, genre: String, _id: false }] })
+	instruments: Instrument[];
 
 	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
 	author: User;
 
-	@Prop({ type: Types.ObjectId, ref: 'Ensemble' }) // not required - only if post is part of an ensemble
-  	ensemble: Ensemble;
+	@Prop({ type: Types.ObjectId, ref: 'Ensemble' }) // Optional ensemble reference
+	ensemble: Ensemble;
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
