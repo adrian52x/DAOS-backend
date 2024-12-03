5d6fda5aa2ec772b6b5992e5400260a912b0c288
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2024-12-03 15:06:18 +0100

    deleting unused imports and comments

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, Body, Put, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Body, Put, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 
