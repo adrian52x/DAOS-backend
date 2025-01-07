44a1cf789176db0e9e396974edc3bbde652a813b
sofia amoroso <sofi79a0@stud.kea.dk>
2025-01-07 11:22:32 +0100

    .

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -9,6 +9,7 @@ import { Model } from 'mongoose';
 describe('AuthController E2E test', () => {
 	let app: INestApplication;
 	let userModel: Model<User>; // Model for the User collection
+	let authToken: string;
 
 	// creating a test environment
 	beforeAll(async () => {
@@ -56,6 +57,51 @@ describe('AuthController E2E test', () => {
 					password: '12',
 				})
 				.expect(400);
+		});
+	});
+
+	// Testing login for an existing user
+	describe('Login an existing user POST /auth/login', () => {
+		const REGISTER_USER_URL = '/auth/register';
+		const LOGIN_USER_URL = '/auth/login';
+
+		// First create a valid user before running login test
+		beforeAll(async () => {
+			await request(app.getHttpServer())
+				.post(REGISTER_USER_URL)
+				.send({
+					name: 'testSofia1',
+					email: 'testSofia1@gmail.com',
+					password: '1234',
+				})
+				.expect(201);
+		});
+
+		// Test login with correct credentials
+		it('should login an existing user and return a toker', async () => {
+			const response = await request(app.getHttpServer())
+				.post(LOGIN_USER_URL)
+				.send({
+					email: 'testSofia1@gmail.com',
+					password: '1234',
+				})
+				.expect(201);
+
+			// Check that the response.body contains a token field. (Jest's toHaveProperty Matcher)
+			expect(response.body).toHaveProperty('access_token');
+			authToken = response.body.access_token; // Store the token for later use
+		});
+	});
+
+	// Testing using the token to access a protected route
+	describe('Access profile with token POST /auth/profile', () => {
+		const PROFILE_ROUTE = '/auth/profile';
+
+		it('should access profile with valid token', () => {
+			// Ensure the token is available before running the test
+			expect(authToken).toBeDefined();
+
+			return request(app.getHttpServer()).get(PROFILE_ROUTE).set('Authorization', `Bearer ${authToken}`).expect(200);
 		});
 	});
 });
