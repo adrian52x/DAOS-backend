89415e3f8985f4004c42d81b7dc8b4dfd4231796
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2025-01-07 18:13:38 +0100

    delete unused stuff

--- src/test.module.ts
+++ src/test.module.ts
@@ -6,25 +6,23 @@ import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './configuration';
-import { Connection } from 'mongoose';
-
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            load: [configuration],
-        }),
-        MongooseModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => ({
-                uri: configService.get<string>('database.test'),
-            }),
-        }),
-        UsersModule,
-        PostsModule,
-        EnsemblesModule,
-        AuthModule,
-    ],
-  })
-  export class TestModule {}
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			load: [configuration],
+		}),
+		MongooseModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				uri: configService.get<string>('database.test'),
+			}),
+		}),
+		UsersModule,
+		PostsModule,
+		EnsemblesModule,
+		AuthModule,
+	],
+})
+export class TestModule {}
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BadRequestException, INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { CreateUserDto } from '../src/users/dto/create-user.dto';
 import { TestModule } from '../src/test.module';
@@ -8,102 +8,87 @@ import { AuthService } from '../src/auth/auth.service';
 import { Response } from 'express';
 
 describe('AuthController (e2e)', () => {
-    let app: INestApplication;
-    let userService: UsersService;
-    let authService: AuthService;
+	let app: INestApplication;
+	let userService: UsersService;
+	let authService: AuthService;
 
-    beforeEach(async () => {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [TestModule],
-        }).compile();
-        userService = moduleFixture.get<UsersService>(UsersService);
-        authService = moduleFixture.get<AuthService>(AuthService);
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [TestModule],
+		}).compile();
+		userService = moduleFixture.get<UsersService>(UsersService);
+		authService = moduleFixture.get<AuthService>(AuthService);
 
-        app = moduleFixture.createNestApplication();
-        app.useGlobalPipes(new ValidationPipe());
+		app = moduleFixture.createNestApplication();
+		app.useGlobalPipes(new ValidationPipe());
 
-        await app.init();
-        await userService.deleteMany();
-    });
+		await app.init();
+		await userService.deleteMany();
+	});
 
-    afterEach(async () => {
-        await app.close();
-    });
+	afterEach(async () => {
+		await app.close();
+	});
 
+	describe('AuthController /auth/register', () => {
+		it('should signup a valid user', async () => {
+			// Arrange
+			const validUser: CreateUserDto = { name: 'TestName', email: 'test@test.dk', password: 'password' };
+			const expectedObject = { name: 'TestName', email: 'test@test.dk' };
 
-    describe('AuthController /auth/register', () => {
-        it('should signup a valid user', async () => {
-            
-            // Arrange
-            const validUser: CreateUserDto = {name: 'TestName', email: 'test@test.dk', password: 'password'};
-            const expectedObject = {name: 'TestName', email: 'test@test.dk'};
+			// Act
+			const { body } = await request(app.getHttpServer()).post('/auth/register').send(validUser).expect(201); // assert
 
-            // Act
-            const  { body }  = await request(app.getHttpServer())
-            .post('/auth/register')
-            .send(validUser)
-            .expect(201) // assert
+			// Assert
+			expect(body._id).toBeDefined();
+			expect(body.name).toBe(expectedObject.name);
+			expect(body.email).toBe(expectedObject.email);
+			expect(body.access_token).toBeDefined();
+			expect(body.password).toBeUndefined(); // Ensure password is not returned in the response
+		});
 
-            // Assert
-            expect(body._id).toBeDefined();
-            expect(body.name).toBe(expectedObject.name);
-            expect(body.email).toBe(expectedObject.email);
-            expect(body.access_token).toBeDefined();
-            expect(body.password).toBeUndefined(); // Ensure password is not returned in the response 
-        });
-        
-        it('should not signup a user with an invalid email', async () => {
-            // Arrange
-            const invalidUser: CreateUserDto = { name: 'TestName', email: 'invalid-email', password: 'password' };
-      
-            // Act
-            const { body } = await request(app.getHttpServer())
-              .post('/auth/register')
-              .send(invalidUser)
-              .expect(400); // assert
-      
-            // Assert
-            expect(body.message).toContain('email must be an email');
-            expect(body.error).toBe('Bad Request');
-            expect(body.statusCode).toBe(400);
-        });
+		it('should not signup a user with an invalid email', async () => {
+			// Arrange
+			const invalidUser: CreateUserDto = { name: 'TestName', email: 'invalid-email', password: 'password' };
 
-        it('should encrypt the user password', async () => {
-            // Arrange
-            const validUser: CreateUserDto = { name: 'TestName', email: 'test@test.dk', password: 'password' };
+			// Act
+			const { body } = await request(app.getHttpServer()).post('/auth/register').send(invalidUser).expect(400); // assert
 
-            // Act
-            await request(app.getHttpServer())
-            .post('/auth/register')
-            .send(validUser)
-            .expect(201); // assert
+			// Assert
+			expect(body.message).toContain('email must be an email');
+			expect(body.error).toBe('Bad Request');
+			expect(body.statusCode).toBe(400);
+		});
 
-            // Assert
-            const createdUser = await userService.findOneByEmail(validUser.email);
-            expect(createdUser).toBeDefined();
-            expect(createdUser.password).not.toBe(validUser.password); // Ensure password is encrypted
-        });
-    });
-  
-    describe('AuthController /auth/login', () => {
-        it('should return a token in cookies when logging in', async () => {
-            // Arrange
-            const res = {} as Response;
-            res.cookie = jest.fn(); // Mock the cookie function
-            await authService.register({ name: 'TestName', email: 'test@test.dk', password: 'password' }, res);
+		it('should encrypt the user password', async () => {
+			// Arrange
+			const validUser: CreateUserDto = { name: 'TestName', email: 'test@test.dk', password: 'password' };
 
-            // Act
-            const response = await request(app.getHttpServer())
-                .post('/auth/login')
-                .send({ email: 'test@test.dk', password: 'password' })
-                .expect(201); // assert
+			// Act
+			await request(app.getHttpServer()).post('/auth/register').send(validUser).expect(201); // assert
 
-            // Assert
-            const cookies = response.headers['set-cookie'];
-            expect(cookies).toBeDefined();
-            const cookieArray = cookies[0].split('; ');
-            expect(cookieArray.some((cookie: string) => cookie.includes('access_token'))).toBe(true);
-        });
-    });
+			// Assert
+			const createdUser = await userService.findOneByEmail(validUser.email);
+			expect(createdUser).toBeDefined();
+			expect(createdUser.password).not.toBe(validUser.password); // Ensure password is encrypted
+		});
+	});
 
+	describe('AuthController /auth/login', () => {
+		it('should return a token in cookies when logging in', async () => {
+			// Arrange
+			const res = {} as Response;
+			res.cookie = jest.fn(); // Mock the cookie function
+			await authService.register({ name: 'TestName', email: 'test@test.dk', password: 'password' }, res);
+
+			// Act
+			const response = await request(app.getHttpServer()).post('/auth/login').send({ email: 'test@test.dk', password: 'password' }).expect(201); // assert
+
+			// Assert
+			const cookies = response.headers['set-cookie'];
+			expect(cookies).toBeDefined();
+			const cookieArray = cookies[0].split('; ');
+			expect(cookieArray.some((cookie: string) => cookie.includes('access_token'))).toBe(true);
+		});
+	});
 });
